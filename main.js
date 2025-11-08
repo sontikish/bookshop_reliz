49188c1fc42113d346bed4c250cb0daa43ccd45e
sontikish <143314559+sontikish@users.noreply.github.com>
2025-11-08 13:19:04 +0200

    day 6

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// Функція для отримання значення кукі за ім'ям
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';');
+
+
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
+
+
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
+        }
+    }
+    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
+    return '';
+}
+
+
+
 // Оголошуємо асинхронну функцію для отримання продуктів з сервера
 async function getProducts() {
     // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
@@ -13,15 +36,17 @@ function getCardHTML(product) {
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-text">${product.price}</p>
-    <a href="#" class="btn btn-primary" data-product='${JSON.stringify(product)}'>В КОРЗИНУ</a>
+    <a href="#" class="btn btn-primary" data-product='${JSON.stringify(product)}'>В КОШИК</a>
   </div>
 </div>`
 }
 
 class ShoppingCart {
     constructor() {
+        
         this.items = {} // об’єкт з товарами у кошику
         this.total = 0  // загальна вартість замовлення
+        this.loadCartFromCookies()
     }
 
     addItem(item) { // Додавання товару до кошика 
@@ -68,14 +93,42 @@ getProducts().then(function (products) {
 
 
     // Отримуємо всі кнопки "Купити" на сторінці
-    let buyButtons = document.querySelectorAll('.products-list .cart-btn');
+    let buyButtons = document.querySelectorAll('.products-list .btn-primary');
     // Навішуємо обробник подій на кожну кнопку "Купити"
     if (buyButtons) {
         buyButtons.forEach(function (button) {
-            button.addEventListener('click', addToCart)
+            button.addEventListener('click', addToCart
+            )
         });
     }
 })
+
+//Оформлення сторінки "Кошик"
+
+let cart_list = document.querySelector(".cart-items-list");
+function get_item(item) {
+  return `<div class = "cart-item">
+                <h4 class="cart-item-title">${item.title}</h4>
+                <div class="cart-item-quantity">Кількість: ${item.quantity }</div>
+                <div class="cart-item-price" data-price="${item.price}">${
+item.price * item.quantity
+  } грн</div>
+            </div>`;
+}
+function showCartList() {
+  //cart_list.innerHTML = "";
+  for (let key in cart.items) {
+    // проходимося по всіх ключах об'єкта cart.items
+    cart_list.innerHTML += get_item(cart.items[key]);
+  }
+  //cart_total.innerHTML = cart.calculateTotal();
+}
+
+
+showCartList();
+
+
+
 
 
 
